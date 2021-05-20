7b6e2fbc9c6117855c5a2b5dc17aba561cba70cd
Lisemara <lisemara765@gmail.com>
2021-05-20 18:40:03 +0900

    main

---
+++ Ch02/02-3-PrintfOne.c
@@ -0,0 +1,17 @@
+/*
+    Author : Lisemara
+    Date   : 21-05-20
+    Page   : 43
+    Summary: printf 함수를 이해하기 위한 예제 코드입니다.
+
+*/
+
+#include <stdio.h>
+
+// 
+int main(void){
+    printf("Hello Everybody\n");
+    printf("%d\n", 1234);
+    printf("%d %d\n", 10, 20);
+    return 0;
+}
