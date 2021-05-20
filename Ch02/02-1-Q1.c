fab9026dd906764c8bf5d3c074671a58ab76b3e8
Lisemara <lisemara765@gmail.com>
2021-05-20 18:35:19 +0900

    first commit

---
+++ Ch02/02-1-Q1.c
@@ -0,0 +1,14 @@
+/*
+    Author : Lisemara
+    Date   : 21-05-20
+    Page   : 39
+    Summary: hello world 코드를 변경하며 연습하는 코드입니다. 
+
+*/
+
+#include <stdio.h>
+
+// 
+int main(void){
+    printf("홍길동\n홍 길 동\n홍  길  동");
+}
---
+++ Ch02/02-1-Q2.c
@@ -0,0 +1,16 @@
+/*
+    Author : Lisemara
+    Date   : 21-05-20
+    Page   : 39
+    Summary: hello world 코드를 변경하며 연습하는 코드입니다. 
+
+*/
+
+#include <stdio.h>
+
+// 
+int main(void){
+    printf("이름 : Lisemara\n");
+    printf("주소 : 서울시 은평구 코큰건물 444호\n");
+    printf("전화번호 : 010-1234-5678\n");
+}
