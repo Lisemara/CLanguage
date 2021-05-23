613d96ab1e5df7ad19c645f49acb157c324d913a
Lisemara <lisemara765@gmail.com>
2021-05-23 23:46:40 +0900

    main

---
+++ Ch04/04-2-Q2.c
@@ -0,0 +1,23 @@
+/*
+    Author : Lisemara
+    Date   : 21-05-23
+    Page   : 83
+    Summary: Q 04-2 - 진법을 이해하기 위해 푸는 문제입니다.
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+    printf("각 바이트들을 10진수로 변환하면?\n");
+    printf("00000001: %d\n", 1);
+    printf("00000010: %d\n", 2);
+    printf("00000100: %d\n", 4);
+    printf("00001000: %d\n", 8);
+    printf("00010000: %d\n", 16);
+    printf("00100000: %d\n", 32);
+    printf("01000000: %d\n", 64);
+    printf("10000000: %d\n", 128);
+ 
+    return 0;
+}
---
+++ Ch04/04-2-Q3.c
@@ -0,0 +1,18 @@
+/*
+    Author : Lisemara
+    Date   : 21-05-23
+    Page   : 83
+    Summary: Q 04-2 - 진법을 이해하기 위해 푸는 문제입니다.
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+    printf("각 바이트들을 10진수로 변환하면?\n");
+    printf("00010001: %d\n", 17);
+    printf("10100010: %d\n", 162);
+    printf("11110111: %d\n", 247);
+ 
+    return 0;
+}
