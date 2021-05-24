c1d19aeb00e65132a5e9d62f7429fde9c726029b
Lisemara <lisemara765@gmail.com>
2021-05-24 23:52:16 +0900

    main

---
+++ Ch04/04-3-Q1.c
@@ -0,0 +1,17 @@
+/*
+    Author : Lisemara
+    Date   : 21-05-23
+    Page   : 89
+    Summary: Q 04-3 - 음과 양의 정수 표현법을 이해하기 위해 푸는 문제입니다.
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+    printf("양의 정수 01001111과 00110011은 10진수로 얼마인가?\n");
+    printf("01001111: %d\n", 79);
+    printf("00110011: %d\n", 51);
+ 
+    return 0;
+}
---
+++ Ch04/04-3-Q2.c
@@ -0,0 +1,17 @@
+/*
+    Author : Lisemara
+    Date   : 21-05-23
+    Page   : 89
+    Summary: Q 04-3 - 음과 양의 정수 표현법을 이해하기 위해 푸는 문제입니다.
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+    printf("음의 정수 10101001과 11110000은 10진수로 얼마인가?\n");
+    printf("10101001: %d\n", -87);
+    printf("11110000: %d\n", -16); 
+ 
+    return 0;
+}
