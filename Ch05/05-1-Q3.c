a7cc55775bbdb731ba8580e09d1566a7163a170c
Lisemara <lisemara765@gmail.com>
2021-05-27 22:42:59 +0900

    main

--- Ch05/05-1-Q3.c
+++ Ch05/05-1-Q3.c
@@ -1,6 +1,6 @@
 /*
     Author : Lisemara
-    Date   : 21-05-26
+    Date   : 21-05-27
     Page   : 119
     Summary: scanf 함수와 ASCII 코드를 활용한 문제입니다.
              ASCII 코드에 대한 질문을 해결하는 문제입니다.
@@ -11,6 +11,8 @@
 
 // 
 int main(void){
-
+    printf("질문 1) 아스키 코드를 구성하는 값의 범위는?: %d ~ %d\n", 0, 127);
+    printf("질문 2) 대문자 A에서부터 대문자 Z까지 증가의 폭은?: %d씩 증가\n", 1);
+    printf("질문 3) 알파벳 대문자와 소문자간의 값의 차는?: %d 차이", 32);
     return 0;
 }
